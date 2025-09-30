239d4f781eefca64fea2faf153b0cc8c67d55450
Pasit Jakkrawankul <pjakkraw@x3002c0s9b0n0.head.lanta.nstda.or.th>
2025-10-01 04:54:38 +0700

    fix metrics names

--- train_multi.sh
+++ train_multi.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH -p gpu                      # Specify partition [Compute/Memory/GPU]
 #SBATCH -N 1 -c 16   			    # Specify number of nodes and processors per task
-#SBATCH --mem=128G
-#SBATCH --ntasks-per-node=1		    # Specify tasks per node
-#SBATCH --gpus=1	                # Specify total number of GPUs
+#SBATCH --ntasks-per-node=4		    # Specify tasks per node
+#SBATCH --gpus=4	                # Specify total number of GPUs
 #SBATCH -t 5:00:00                # Specify maximum time limit (hour: minute: second)
 #SBATCH -A lt200377               	# Specify project name
 #SBATCH -J eomt               	# Specify job name
@@ -17,10 +16,21 @@ export HF_HUB_CACHE="/home/pjakkraw/.cache/huggingface/hub"
 export HF_DATASETS_OFFLINE=1
 export HF_HUB_OFFLINE=1
 
-python main.py fit \
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+echo "MASTER_PORT="$MASTER_PORT
+
+export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
+echo "WORLD_SIZE="$WORLD_SIZE
+
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+echo "MASTER_ADDR="$MASTER_ADDR
+
+srun python main.py fit \
   -c configs/dinov3/coco/instance/eomt_small_640.yaml \
-  --trainer.devices 1 \
-  --trainer.accumulate_grad_batches 8 \
-  --data.num_workers 0 \
-  --data.batch_size 8 \
+  --compile_disabled \
+  --trainer.devices 4 \
+  --trainer.num_nodes 1 \
+  --data.num_workers 2 \
+  --data.batch_size 32 \
   --data.path /project/lt200377-mpind/segment
